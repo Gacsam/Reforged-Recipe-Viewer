3c48e93bf76eb29e80328ee288b3c269aa97db65
Gacsam <gacsam@ymail.com>
2022-12-20 10:41:48 +0000

    Files

---
+++ IngredientForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace CraftingTool
+{
+    partial class IngredientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 199);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(246, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(384, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X 50";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 292);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 57);
+            this.textBox1.TabIndex = 5;
+            // 
+            // IngredientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 373);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "IngredientForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace CraftingTool
+{
+    
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>        
+        [STAThread]
+        
+        static void Main()
+        {            
+            char[] charSeparators = new char[] { '\n' };
+            // load ItemList from Resources and split at new line
+            List<ItemWithImage> itemsWithImages = new List<ItemWithImage>();
+            string[] allItemsCSV = Properties.Resources.ItemList.Replace("\r", "").Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in allItemsCSV)
+            {
+                if (item.Contains("itemName"))
+                    continue;
+                // Get rid of the bloody \r and split into two columns
+                string[] columns = item.TrimEnd('\r').Split(',');
+                itemsWithImages.Add(new ItemWithImage(columns[0], columns[1]));
+            }
+
+            List<BaseItemConvert> baseItems = new List<BaseItemConvert>();
+            string[] baseItemsCSV = Properties.Resources.BaseItemConversion.Replace("\r", "").Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in baseItemsCSV) 
+            {
+                if (item.Contains("BaseItem"))
+                    continue;
+
+                string[] columns = item.TrimEnd('\r').Split(',');
+                ItemWithImage baseItem = itemsWithImages.Find(x => x.itemName.Contains(columns[0]));
+                ItemWithImage craftedItem = itemsWithImages.Find(x => x.itemName.Contains(columns[2]));
+                baseItems.Add(new BaseItemConvert(baseItem, columns[1], craftedItem, columns[3], columns[4]));
+            }
+
+            string[] craftingCSV = Properties.Resources.ItemRecipes.Replace("\r", "").Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+            List<CraftingRecipe> craftingRecipes = new List<CraftingRecipe>();
+            foreach (string item in craftingCSV)
+            {
+                if (item.Contains("Recipe"))
+                    continue;
+
+                string[] columns = item.Split(',');
+                ItemWithImage craftedItem = itemsWithImages.Find(x => x.itemName.Equals(columns[0]));
+                ItemWithImage ingredientOne = itemsWithImages.Find(x => x.itemName.Equals(columns[2]));
+                ItemWithImage ingredientTwo = itemsWithImages.Find(x => x.itemName.Equals(columns[4]));
+                ItemWithImage ingredientThree = itemsWithImages.Find(x => x.itemName.Equals(columns[6]));
+                ItemWithImage ingredientFour = itemsWithImages.Find(x => x.itemName.Equals(columns[8]));
+                craftingRecipes.Add(new CraftingRecipe(craftedItem, columns[1], ingredientOne, columns[3], ingredientTwo, columns[5], ingredientThree, columns[7], ingredientFour, columns[9], columns[10]));
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CraftForm(craftingRecipes, baseItems));
+
+        }
+    }
+
+    public class ItemWithImage
+    {
+        public string itemName;
+        public string imageDir;
+
+        public ItemWithImage(string item, string image)
+        {
+            itemName = item;
+            imageDir = image;
+        }
+    }
+
+    public class BaseItemConvert
+    {
+
+        public ItemWithImage ingredientItem;
+        public string ingredientCount;
+        public ItemWithImage resultItem;
+        public string resultCount;
+        public string description;
+        public BaseItemConvert(ItemWithImage ingredientItem, string ingredientCount, ItemWithImage resultItem, string resultCount, string description)
+        {
+            this.ingredientItem = ingredientItem;
+            this.ingredientCount = ingredientCount;
+            this.resultItem = resultItem;
+            this.resultCount = resultCount;
+            this.description = description;
+        }
+    }
+
+    public class CraftingRecipe
+    {
+        public ItemWithImage craftedItem;
+        public string craftCount;
+        public ItemWithImage ingredientOneItem;
+        public string ingredientOneCount;
+        public ItemWithImage ingredientTwoItem;
+        public string ingredientTwoCount;
+        public ItemWithImage ingredientThreeItem;
+        public string ingredientThreeCount;
+        public ItemWithImage ingredientFourItem;
+        public string ingredientFourCount;
+        public string category;
+        public CraftingRecipe(ItemWithImage craftedItem, string craftCount, ItemWithImage ingredientOneItem, string ingredientOneCount, ItemWithImage ingredientTwoItem, string ingredientTwoCount, ItemWithImage ingredientThreeItem, string ingredientThreeCount, ItemWithImage ingredientFourItem, string ingredientFourCount, string category)
+        {
+            this.craftedItem = craftedItem;
+            this.craftCount = craftCount;
+            this.ingredientOneItem = ingredientOneItem;
+            this.ingredientOneCount = ingredientOneCount;
+            this.ingredientTwoItem = ingredientTwoItem;
+            this.ingredientTwoCount = ingredientTwoCount;
+            this.ingredientThreeItem = ingredientThreeItem;
+            this.ingredientThreeCount = ingredientThreeCount;
+            this.ingredientFourItem = ingredientFourItem;
+            this.ingredientFourCount = ingredientFourCount;
+            this.category = category;
+        }
+    }
+}
